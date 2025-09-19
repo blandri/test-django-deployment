750e73a83dbb709d03671dc00355b74596a46782
Landry  Bishinga <landrybishinga@Irembo-182-LBishinga.local>
2025-09-19 13:56:15 +0200

    x

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,14 +6,5 @@ urlpatterns = [
     # path('register', views.register, name='register'),
     
     # Document management
-    path('documents', views.list_documents, name='list_documents'),
-    path('documents/create', views.create_document, name='create_document'),
-    # path('documents/bulk-create/', views.bulk_create_documents, name='bulk_create_documents'),
-    # path('documents/<int:document_id>/delete/', views.delete_document, name='delete_document'),
-    
-    # Vector search
-    # path('search/semantic/', views.semantic_search, name='semantic_search'),
-    # path('search/analytics/', views.search_analytics, name='search_analytics'),
-    path('agent/create/testcase', views.create_testcase, name='create_testcase'),
-    path("download/testcases", views.download_file, name="download_file"),
+    path('documents', views.create_document, name='list_documents')
 ]
