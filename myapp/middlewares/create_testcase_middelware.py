580381b82f699fcc5b45993561df6381668f1c89
Landry  Bishinga <landrybishinga@Irembo-182-LBishinga.local>
2025-09-20 11:53:54 +0200

    sdd2

---
+++ myapp/middlewares/create_testcase_middelware.py
@@ -0,0 +1,46 @@
+# from ..services.notion_service import NotionClient
+# from ..services.rag_service import RAGService
+# from ..services.gemma_service import GemmaServ
+from ..helpers.excel_generator import ExcelGenerator
+# from ..services.groq_service import GroqApi
+from mysite import settings
+import os
+import shutil
+
+class CreateTestCaseMiddleWare:
+    def __init__(self, pageId):
+        self.notionPageId = pageId
+        # self.notionClient = NotionClient(settings.NOTION_API_KEY)
+        # self.rag = RAGService()
+        # self.gemma = GemmaServ()
+        # self.groq = GroqApi()
+        self.excelGenerator = ExcelGenerator()
+        self.imagesDir = "images"
+        self.testcasesDir = "files"
+
+    def testcase_generation (self):
+        # shutil.rmtree(self.testcasesDir)
+
+        # res = self.notionClient.notion_to_markdown(self.notionPageId)
+
+        # files = os.listdir(self.imagesDir)
+        
+        # workflowInfo = self.gemma.query_gemma('Analyse this workflow diagram and generate texts explaining all the actions and events in it, respond with a numbered list.', f"{self.imagesDir}/{files[0]}")
+
+        # new_res = res.replace('Image_placeholder', f'''{workflowInfo}''')
+
+        # analysed_srd = self.rag.analyze_srd_data(jk)
+
+        # similarDocuments = self.rag.retrieve_similar_content("Find similar testcases with this service", analysed_srd, 20)
+        # # print('++++', similarDocuments)
+        # # Gemma
+        # cases = self.gemma.generate_cases(analysed_srd, similarDocuments)
+
+        # # groq
+        # # cases = self.groq.generate_testcases(analysed_srd, similarDocuments)
+        # print('>>>', cases)
+        # excelFile = self.excelGenerator.generate_testcase_excel(cases, "Semen")
+
+        # shutil.rmtree(self.imagesDir)
+
+        return 'excelFile'
